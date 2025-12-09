0e59f9555174c5b985b13872d5a277ac36e0bbb0
Padam1060 <pappadam-10388@taszi.hu>
2025-12-09 10:03:31 +0100

    Update index.js

--- videoghoposcuc/index.js
+++ videoghoposcuc/index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 
 
 import userRoutes from "./routes/userRouter.js";
+import postRoutes from "./routes/postRouter.js";
+import commentRouter from "./routes/commentRouter.js";
 
 const app=express();
 
@@ -11,7 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 app.use('/api', userRoutes);
-
+app.use('/api', postRoutes)
+app.use('/api', commentRouter)
 const PORT = 8000;
 app.listen(PORT, () => {
   console.log(` Server running on http://localhost:${PORT}`);
